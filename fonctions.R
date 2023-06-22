c6b3bdaddfc970922e5f2c81f7ceb12cfa103d4f
Nathalie Dubreu <nathalie.dubreu@yahoo.fr>
2023-06-21 14:25:34 -1000

    Initialisation projet

---
+++ fonctions.R
@@ -0,0 +1,5 @@
+readCSV <- function(nomFichier, chemin = Sys.getenv("cheminAcces")) {
+  readr::read_csv2(file.path(chemin,nomFichier))
+}
+
+# |>
---
+++ master.R
@@ -0,0 +1,3 @@
+library("dplyr")
+
+source("fonctions.R")
