ab6681a25e67fdcffe03670c327fb74d141023e9
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-08-22 15:50:20 -1000

    Add files via upload

--- eligibles.R
+++ eligibles.R
@@ -4,9 +4,30 @@ eligiblesRGA <- exportRGA |>
     (eligibilite == 1 & (substring(id_exploitation, 0, 1) == "P" | substring(id_exploitation, 0, 1) == "M")) |
     ((eligibilite == 1 | eligibiliteCoprah == 1) & substring(id_exploitation, 0, 1) == "X"))
 
+## Cultivateurs
+
 eligiblesCultivateurs <- eligiblesRGA |>
   filter(RaisonsRecensement__1 == 1)
 
 cultivateurs <- eligiblesCultivateurs |>
   mutate(SAU = case_when(is.na(SurfaceTotalProdAgri) ~ 0, TRUE ~ as.numeric(SurfaceTotalProdAgri))) |>
   summarise(SAU_totale_hectare = sum(SAU)/10000, SAU_moyenne_hectare = mean(SAU)/10000)
+
+
+## Cultivateurs
+
+eligiblesEleveurs <- eligiblesRGA |>
+  filter(RaisonsRecensement__2 == 1)
+
+
+# Bovins........................1/1
+# Ovins.........................2/2
+# Caprins.......................8/8
+# Porcins.......................3/3
+# Volailles.....................4/4
+# Equidés.......................5/5
+# Lapins élevés pour la chair...6/6
+# Abeilles......................7/7
+
+eleveursBovins <- eligiblesEleveurs |>
+  filter(PresenceAnimaux__1 == 1)
