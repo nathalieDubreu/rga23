9a07116ca52674f557a1d9a2cee2db672461a30e
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-08-24 13:51:00 -1000

    Add files via upload

---
+++ analyse/coprahculteurs.R
@@ -0,0 +1,17 @@
+source("analyse/eligibles.R")
+
+## Coprahculteurs éligibles
+eligiblesCoprahculteurs <- eligiblesRGA |>
+  filter(RaisonsRecensement__3 == 1)
+
+
+cocoteraiesExploitees <- eligiblesCoprahculteurs |> summarize(`Nb de producteurs de coprah` = n(), 
+                                     `Nb de cocoteraies exploitées` = sum(NbCocoteraies),  
+                                     `- en tant que propriétaire plein` = sum(nbCocoStatut1),
+                                     `- en tant que propriétaire en indivision` = sum(nbCocoStatut2),
+                                     `- en tant qu'exploitant` = sum(nbCocoStatut3),
+                                     `Nb moyen de cocoteraies` = mean(NbCocoteraies),
+                                     `Nb min de cocoteraies` = min(NbCocoteraies),
+                                     `Nb max de cocoteraies` = max(NbCocoteraies))
+
+writeCSV(cocoteraiesExploitees)
