b547e393b24fa4d83740f1adca0022862ba8c8d7
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-09-05 10:16:02 -1000

    Add files via upload

--- master.R
+++ master.R
@@ -3,17 +3,19 @@ library("dplyr")
 source("fonctions.R")
 
 # TODO - mettre à jour le nom du dossier contenant l'export dézippé de SuSo
-dossier <- "Export_09-04"
+dossier <- "Export_09-05"
 
 # Suivi de la collecte
 ## Mise à jour du fichier Rmd
-date <- "04/09/2023"
+date <- "05/09/2023"
 source("suiviCollecte/comptages.R")
 
 # Mises en forme des données (à lancer dans l'ordre)
+source("miseEnForme/imputationVariables.R")
 source("miseEnForme/rostersSurfaces.R")
 source("miseEnForme/decoupageRga23.R")
 source("miseEnForme/rostersCommercialisation.R")
+source("miseEnForme/rosterMainOeuvre.R")
 
 # Analyse
 ## Non éligibles
