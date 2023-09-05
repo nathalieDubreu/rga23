2fb8df03872d0e05afe539235b490af6de65601b
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-09-05 12:06:48 -1000

    Add files via upload
    Imputation PresSurfIrrigables + SurfaceIrrigJardins + SurfaceBioJardins

--- miseEnForme/imputationVariables.R
+++ miseEnForme/imputationVariables.R
@@ -13,14 +13,31 @@ rga23Brut <- readTable("rga23.tab", dossier) |>
     (ProvenanceSemences__2 == 0) ~ 1,
     (ProvenanceSemences__2 == 1 & is.na(PartSemencesAutoP)) ~ 5,
     TRUE ~ PartSemencesAutoP
-  ))|>
+  )) |>
   mutate(PartRevenusAgriExpl = case_when(
     (ActivitesChefExploit__1 == 1 & is.na(PartRevenusAgriExpl)) ~ 4,
     TRUE ~ PartRevenusAgriExpl
+  )) |>
+  mutate(PresSurfIrrigables = case_when(
+    (Irrigation == 1) ~ 1,
+    TRUE ~ PresSurfIrrigables
+  )) |>
+  mutate(SurfaceBioJardins = case_when(
+    (AgriBio == 1 & !is.na(SurfaceJardins)) ~ 1,
+    (AgriBio == 2 & !is.na(SurfaceJardins)) ~ 2,
+    TRUE ~ SurfaceBioJardins
+  )) |>
+  mutate(SurfaceIrrigJardins = case_when(
+    (Irrigation == 2 & !is.na(SurfaceJardins)) ~ 0,
+    TRUE ~ SurfaceIrrigJardins
   ))
 
 rga23 <- rga23Brut
-  
 
-rga23Brut |> group_by(PartRevenusAgriExpl) |> count()
-rga23Brut |> group_by(PartRevenusAgriExpl2) |> count()
+
+rga23Brut |>
+  group_by(SurfaceIrrigJardins) |>
+  count()
+rga23Brut |>
+  group_by(SurfaceIrrigJardins2) |>
+  count()
