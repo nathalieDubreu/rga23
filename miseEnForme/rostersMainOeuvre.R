6e089a44a59f99b256fce2775e9cb395e0a37407
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-09-12 12:41:47 -1000

    Add files via upload
    Récupération des valeurs du roster pour les mettre dans le fichier correspondant

--- miseEnForme/rostersMainOeuvre.R
+++ miseEnForme/rostersMainOeuvre.R
@@ -1,7 +1,32 @@
 rga23_coexploitants <- readTable("RosterCoExploit.tab", dossier) |> select(!interview__id)
-rga23_moPermanenteNonFam <- readTable("MONonFamPerm.tab", dossier) |> select(!interview__id)
 rga23_moPermanenteFam <- readTable("RosterMOPermFam.tab", dossier) |> select(!interview__id)
 
 writeCSVTraites(rga23_coexploitants)
-writeCSVTraites(rga23_moPermanenteNonFam)
 writeCSVTraites(rga23_moPermanenteFam)
+
+# Récupération des valeurs du roster sur la main d'oeuvre permanente non familiale dans la table rga_mainOeuvre
+
+rosterMoPermanenteNonFam <- readTable("MONonFamPerm.tab", dossier) |> select(!interview__id)
+
+head(rosterMoPermanenteNonFam)
+
+for (i in 1:4) {
+  nBFemmesI <- paste("nbFemMONonFamPerm__", i, sep = "")
+  nBHommesI <- paste("nbHomMONonFamPerm__", i, sep = "")
+
+  TableI <-
+    rosterMoPermanenteNonFam |> filter(MONonFamPerm__id == i)
+
+  rga23_mainOeuvre <-
+    left_join(
+      rga23_mainOeuvre,
+      TableI |> select(!MONonFamPerm__id),
+      by = c("interview__key")
+    ) |>
+    rename(
+      "{nBFemmesI}" := MOFemEmpl,
+      "{nBHommesI}" := MOHomEmpl
+    )
+}
+
+writeCSVTraites(rga23_mainOeuvre)
