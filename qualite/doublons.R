c91efce117b35c67d564ec20ea02b7403e728699
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-10-12 16:59:40 -1000

    Ajout statut 130 - validation QG

--- qualite/doublons.R
+++ qualite/doublons.R
@@ -1,7 +1,19 @@
 doublonsNonInterroges <- readCSV("rga23.csv") |>
-  filter(statut_collecte == 5 & interview__status == "120") |>
+  filter(statut_collecte == 5 & (interview__status == "120" | interview__status == "130")) |>
   select(interview__key, id_exploitation, assignment__id) |>
   rename(idExploitDoublon = id_exploitation, numAffectationDoublon = assignment__id)
 
 commentaires <- left_join(doublonsNonInterroges, readTable("interview__comments.tab", dossier)) |>
   select(!interview__id & !roster & !id1 & !id2)
+
+
+## Et ceux qui n'existent plus...
+# existePlus <- readCSV("rga23.csv") |>
+#   filter(statut_collecte == 2 & (interview__status == "120" | interview__status == "130")) |>
+#   select(interview__key, id_exploitation, assignment__id) |>
+#   rename(idExploitDoublon = id_exploitation, numAffectationDoublon = assignment__id)
+# 
+# commentairesExistePlus <- left_join(existePlus, readTable("interview__comments.tab", dossier)) |>
+#   select(!interview__id & !roster & !id1 & !id2)
+
+
