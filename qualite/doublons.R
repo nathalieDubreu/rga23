1405bfbdb24ef23bb6e31d075f46cdd861701667
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-11-09 13:26:22 -1000

    Add files via upload

--- qualite/doublons.R
+++ qualite/doublons.R
@@ -3,10 +3,22 @@ doublonsNonInterroges <- readCSV("rga23.csv") |>
   select(interview__key, id_exploitation, assignment__id, id_enqueteur_ech) |>
   rename(idExploitDoublon = id_exploitation, numAffectationDoublon = assignment__id)
 
-commentaires <- left_join(doublonsNonInterroges, readTable("interview__comments.tab", dossier)) |>
+commentaires <- left_join(nouveauxDoublonsNonInterroges, readTable("interview__comments.tab", dossier)) |>
   select(!interview__id & !roster & !id1 & !id2)
 
 # writeCSV(commentaires)
+
+## A ajouter dans le fichier doublons_vX.csv et augmenter la version pour la lecture suivante
+nouveauxDoublonsNonInterroges <- anti_join(doublonsNonInterroges, readCSV("doublons_v2.csv") |>
+                                             select(interview__keyDoublon) |>
+                                             rename(interview__key = interview__keyDoublon))
+
+## A supprimer si besoin et augmenter la version pour la lecture suivante
+anciensDoublonsNonInterroges <- anti_join(readCSV("doublons_v2.csv") |> 
+                                             select(interview__keyDoublon) |>
+                                             rename(interview__key = interview__keyDoublon),doublonsNonInterroges)
+
+# writeCSV(nouveauxDoublonsNonInterroges)
 
 ## Et ceux qui n'existent plus...
 # existePlus <- readCSV("rga23.csv") |>
@@ -17,7 +29,7 @@ commentaires <- left_join(doublonsNonInterroges, readTable("interview__comments.
 # commentairesExistePlus <- left_join(existePlus, readTable("interview__comments.tab", dossier)) |>
 #   select(!interview__id & !roster & !id1 & !id2)
 
-doublonsManquants <- readCSV("doublons_v2.csv") |>
+doublonsManquants <- readCSV("doublons_v3.csv") |>
   filter(is.na(idExploit) & is.na(numAffectation)) |>
   select(interview__keyDoublon, idExploitDoublon, numAffectationDoublon, comment, TODO) |>
   rename(interview__key = interview__keyDoublon)
