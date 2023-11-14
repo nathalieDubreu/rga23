3ec4eff2ca0bedb197249d1df3b01f209fb6a49b
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-11-14 11:32:49 -1000

    Ajout avertissements - collecte encore en cours

--- champs/indicatricesAppartenances.R
+++ champs/indicatricesAppartenances.R
@@ -1,6 +1,9 @@
 source("champs/champRGA.R")
 source("champs/champ2012.R")
 
+# Pour le copil : restriction aux questionnaires validés
+# rga23 <- rga23 |> filter((interview__status == 130 | interview__status == 120))
+
 # Indicatrices d'appartenance au champ 2023
 rga23A <- left_join(rga23, idExploitantsDansLeChamp |> mutate(ValideRGA = 1),
   by = c("interview__key")
