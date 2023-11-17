5b4d172368c43e3435c5aa4fc6601067bced7e85
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-11-16 16:05:08 -1000

    Add files via upload

--- qualite/fichiersTestSIA.R
+++ qualite/fichiersTestSIA.R
@@ -1,3 +1,5 @@
+source("champs/champRGA.R")
+
 rga23 <- readCSV("rga23.csv")
 
 rga23Filtre <- rga23 |> filter((((eligibiliteCoprah == 1 & substring(id_exploitation, 0, 2) == "C1" & nchar(id_exploitation) == 5 & !grepl("5", id_exploitation) & !grepl("6", id_exploitation)) |
@@ -21,22 +23,57 @@ inner_join(rga23Filtre, idExploitantsDansLeChamp) |>
 idRGAFiltre <- rga23Filtre |> select(interview__key)
 
 selectionTest <- function(table) {
-  table <- inner_join(table, idRGAFiltre)
+  table <- inner_join(table, idRGAFiltre, by = c("interview__key"))
   return(table)
 }
 
-rga23_gestion <- selectionTest(rga23_gestion)
-rga23_peche <- selectionTest(rga23_peche)
-rga23_coprahculteurs <- selectionTest(rga23_coprahculteurs)
-rga23_exploitations <- selectionTest(rga23_exploitations)
-rga23_general <- selectionTest(rga23_general)
-rga23_mainOeuvre <- selectionTest(rga23_mainOeuvre)
-rga23_prodAnimales <- selectionTest(rga23_prodAnimales)
-rga23_prodVegetales <- selectionTest(rga23_prodVegetales)
-rga23_tape <- selectionTest(rga23_tape)
-
-writeCSVTraites(rga23_gestion, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
-writeCSVTraites(rga23_peche, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
+# rga23_gestion <- selectionTest(readCSV("rga23_gestion.csv"))
+# rga23_peche <- selectionTest(readCSV("rga23_peche.csv"))
+rga23_coprahculteurs <- selectionTest(readCSV("rga23_coprahculteurs.csv"))
+rga23_exploitations <- selectionTest(readCSV("rga23_exploitations.csv"))
+rga23_general <- selectionTest(readCSV("rga23_general.csv")) |> select(
+  !SituationConjChefExpl &
+    !ActiviteConjoint__1 &
+    !ActiviteConjoint__2 &
+    !ActiviteConjoint__3 &
+    !ActiviteConjoint__4 &
+    !ActiviteConjoint__5 &
+    !ActiviteConjoint__6 &
+    !ActiviteConjoint__7 &
+    !ActiviteConjoint__8 &
+    !ActiviteConjoint__9 &
+    !ActiviteConjoint__10 &
+    !ActiviteConjoint__11 &
+    !ActiviteConjoint__12
+)
+rga23_mainOeuvre <- selectionTest(readCSV("rga23_mainOeuvre.csv"))
+rga23_prodAnimales <- selectionTest(readCSV("rga23_prodAnimales.csv"))
+rga23_prodVegetales <- selectionTest(readCSV("rga23_prodVegetales.csv"))
+rga23_tape <- selectionTest(readCSV("rga23_tape.csv")) |> select(
+  !PropTravailPenibleExpl &
+    !AvenirAgri &
+    !MoyensCompetences &
+    !BesoinsSatisf &
+    !Economies &
+    !FreqEvenComLocale &
+    !ProbActivite__1 &
+    !ProbActivite__2 &
+    !ProbActivite__3 &
+    !ProbActivite__4 &
+    !NoteSatisfactionExpl &
+    !ConnaissPratiques &
+    !InteretPratiques &
+    !ConnaissOrga &
+    !FruitsLocaux &
+    !PoissonsLocaux &
+    !LegumesLocaux &
+    !VivriLocaux &
+    !PlatsTradi &
+    !MaaTahitiPlus &
+    !BesoinsNutri
+)
+# writeCSVTraites(rga23_gestion, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
+# writeCSVTraites(rga23_peche, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 writeCSVTraites(rga23_coprahculteurs, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 writeCSVTraites(rga23_exploitations, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 writeCSVTraites(rga23_general, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
@@ -45,13 +82,13 @@ writeCSVTraites(rga23_prodAnimales, file.path(Sys.getenv("cheminAcces"), "TestSI
 writeCSVTraites(rga23_prodVegetales, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 writeCSVTraites(rga23_tape, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 
-rga23_cocoteraies <- selectionTest(rga23_cocoteraies)
-rga23_coexploitants <- selectionTest(rga23_coexploitants)
-rga23_engraisOrganiques <- selectionTest(rga23_engraisOrganiques)
-rga23_moPermanenteFam <- selectionTest(rga23_moPermanenteFam)
-rga23_parcelles <- selectionTest(rga23_parcelles)
-rga23_sites <- selectionTest(rga23_sites)
-rga23_surfacesCultures <- selectionTest(rga23_surfacesCultures)
+rga23_cocoteraies <- selectionTest(readCSV("rga23_cocoteraies.csv"))
+rga23_coexploitants <- selectionTest(readCSV("rga23_coexploitants.csv"))
+rga23_engraisOrganiques <- selectionTest(readCSV("rga23_engraisOrganiques.csv"))
+rga23_moPermanenteFam <- selectionTest(readCSV("rga23_moPermanenteFam.csv"))
+rga23_parcelles <- selectionTest(readCSV("rga23_parcelles.csv"))
+rga23_sites <- selectionTest(readCSV("rga23_sites.csv"))
+rga23_surfacesCultures <- selectionTest(readCSV("rga23_surfacesCultures.csv"))
 
 writeCSVTraites(rga23_cocoteraies, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
 writeCSVTraites(rga23_coexploitants, file.path(Sys.getenv("cheminAcces"), "TestSIA"))
