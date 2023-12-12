aee08ca946edb778719161479bed9fa2d1652474
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2023-12-12 13:18:07 -1000

    Add files via upload

--- miseEnForme/ajoutsDonneesX.R
+++ miseEnForme/ajoutsDonneesX.R
@@ -3,10 +3,24 @@
 
 # WIP
 
-donneesMO <- readInputCSV("donneesMainOeuvre.csv")
+donneesMO <- readInputCSV("donneesMainOeuvre.csv") |> select(!id_exploitation)
 
-# rm(
-#   donneesMO
-# )
+rga23AvecDonneesX <- left_join(rga23, donneesMO, by=c("interview__key"), suffix = c("_rga23", "_donneesMO"))
+
+# Liste des variables communes à traiter
+variables_comunes <- setdiff(intersect(names(rga23), names(donneesMO)), "interview__key")
+
+# Appliquer la condition pour chaque variable commune
+for (variable in variables_comunes) {
+  condition <- paste0(variable, " = ifelse(!is.na(", variable, "_donneesMO), ", variable, "_donneesMO, ", variable, "_rga23)")
+  rga23AvecDonneesX <- rga23AvecDonneesX |> mutate(!!variable := eval(parse(text = condition)))
+}
+
+rga23 <- rga23AvecDonneesX |> select(-ends_with("_rga23")) |> select(-ends_with("_donneesMO"))
+
+rm(
+  donneesMO,
+  rga23AvecDonneesX
+)
 
 writeCSVTraites(rga23)
