296d07160ef9bd83ecd2b24a23014ca3b9b71c30
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-01-15 12:07:01 -1000

    Suppression doublons SuSo (en dur)

--- suiviCollecte/comptages.R
+++ suiviCollecte/comptages.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 library(knitr)
 
-exportRGA <- readTable("rga23.tab", dossier)
+exportRGA <- readTable("rga23.tab", dossier) |> filter(interview__key != "59-36-31-34" & interview__key != "06-79-34-97")
 iles <- readInputCSV("iles.csv")
 
 rmarkdown::render("suiviCollecte/comptagesRga23.Rmd")
