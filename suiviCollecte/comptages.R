a5c5f6fc82e8575d86ea7926c85064f6f6e47ed7
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-01-15 12:29:05 -1000

    Suppression doublons affectations (en dur)

--- suiviCollecte/comptages.R
+++ suiviCollecte/comptages.R
@@ -1,7 +1,8 @@
 library(rmarkdown)
 library(knitr)
 
-exportRGA <- readTable("rga23.tab", dossier) |> filter(interview__key != "59-36-31-34" & interview__key != "06-79-34-97")
+exportRGA <- readTable("rga23.tab", dossier) |>
+  filter(interview__key != "59-36-31-34" & interview__key != "06-79-34-97" & interview__key != "26-72-53-00" & interview__key != "49-29-35-86")
 iles <- readInputCSV("iles.csv")
 
 rmarkdown::render("suiviCollecte/comptagesRga23.Rmd")
