969b46c7eeb1ec8b207ba7ca503148622665daeb
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-01-26 15:08:48 -1000

    WIP - bâtiments : surface et capacité

--- divers/valeursExtremes.R
+++ divers/valeursExtremes.R
@@ -30,6 +30,9 @@ valExtremes(totalSurfaceFourrages, 0.97)
 valExtremes(totalSurfaceJacheres, 0.99)
 valExtremes(totalSurfaceFlorale, 0.99)
 
+## Surfaces de jardins océaniens
+valExtremes(SurfaceJardins, 0.96)
+
 ## Cheptels les plus importants
 valExtremes(nbTotalBovins, 0.95)
 valExtremes(nbTotalEquides, 0.9)
@@ -90,3 +93,121 @@ rga23_prodAnimales |>
 valExtremes(ProductionRuches, 0.98)
 valExtremes(ProductionPoules1, 0.95)
 valExtremes(ProductionPoules3, 0.95)
+
+# Batiments et parcours
+
+## Bovins
+rga23_prodAnimales |>
+  filter(CapaciteBatLogtBovins > 0 | SuperficieBatLogtBovins > 0) |>
+  select(interview__key, CapaciteBatLogtBovins, SuperficieBatLogtBovins, nbTotalBovins)
+# rga23_prodAnimales |> filter(CapaciteBatTraiteBovins>0)
+# rga23_prodAnimales |> filter(SuperficieBatTraiteBovins>0)
+# rga23_prodAnimales |> filter(CapaciteAutreBatBovins>0)
+# rga23_prodAnimales |> filter(SuperficieAutreBatBovins>0)
+rga23_prodAnimales |>
+  filter(SurfacePaturageBovins > 0) |>
+  select(interview__key, SurfacePaturageBovins, NbBovinsPaturage)
+
+## Caprins
+rga23_prodAnimales |>
+  filter(CapaciteBatLogtCaprins > 0 | SuperficieBatLogtCaprins > 0) |>
+  select(interview__key, CapaciteBatLogtCaprins, SuperficieBatLogtCaprins, nbTotalCaprins)
+# rga23_prodAnimales |> filter(CapaciteBatTraiteCaprins>0)
+# rga23_prodAnimales |> filter(SuperficieBatTraiteCaprins>0)
+rga23_prodAnimales |>
+  filter(CapaciteAutreBatCaprins > 0 | SuperficieAutreBatCaprins > 0) |>
+  select(interview__key, CapaciteAutreBatCaprins, SuperficieAutreBatCaprins, nbTotalCaprins)
+rga23_prodAnimales |>
+  filter(SurfacePaturageCaprins > 0) |>
+  select(interview__key, SurfacePaturageCaprins, NbCaprinsPaturage)
+
+## Equides
+rga23_prodAnimales |>
+  filter(CapaciteBatLogtEquides > 0 | SuperficieBatLogtEquides > 0) |>
+  select(interview__key, CapaciteBatLogtEquides, SuperficieBatLogtEquides, nbTotalEquides)
+rga23_prodAnimales |>
+  filter(CapaciteAutreBatEquides > 0 | SuperficieAutreBatEquides > 0) |>
+  select(interview__key, CapaciteAutreBatEquides, SuperficieAutreBatEquides, nbTotalEquides)
+rga23_prodAnimales |>
+  filter(SurfacePaturageEquides > 0) |>
+  select(interview__key, SurfacePaturageEquides, NbEquidesPaturage)
+
+## Ovins
+rga23_prodAnimales |>
+  filter(CapaciteBatLogtOvins > 0 | SuperficieBatLogtOvins > 0) |>
+  select(interview__key, CapaciteBatLogtOvins, SuperficieBatLogtOvins, nbTotalOvins)
+# rga23_prodAnimales |> filter(CapaciteAutreBatOvins>0)
+# rga23_prodAnimales |> filter(SuperficieAutreBatOvins>0)
+rga23_prodAnimales |>
+  filter(SurfacePaturageOvins > 0) |>
+  select(interview__key, SurfacePaturageOvins, NbOvinsPaturage)
+
+## Porcins
+rga23_prodAnimales |>
+  filter(CapaciteCabanesPorcs > 0 | SurfaceCabanesPorcs > 0) |>
+  select(interview__key, CapaciteCabanesPorcs, SurfaceCabanesPorcs, nbTotalPorcs)
+rga23_prodAnimales |>
+  filter(CapaciteParcCochons > 0 | SurfaceParcCochons > 0) |>
+  select(interview__key, CapaciteParcCochons, SurfaceParcCochons, nbTotalPorcs)
+rga23_prodAnimales |>
+  filter(CapacitePorcherieCaille > 0 | SurfacePorcherieCaille > 0) |>
+  select(interview__key, CapacitePorcherieCaille, SurfacePorcherieCaille, nbTotalPorcs)
+rga23_prodAnimales |>
+  filter(CapacitePorcherieTrad > 0 | SurfacePorcherieTrad > 0) |>
+  select(interview__key, CapacitePorcherieTrad, SurfacePorcherieTrad, nbTotalPorcs)
+rga23_prodAnimales |>
+  filter(SurfaceParcoursPorcins > 0) |>
+  select(interview__key, SurfaceParcoursPorcins, nbTotalPorcins)
+
+## Poules
+
+### 0 - bio
+rga23_prodAnimales |>
+  filter(CapaciteMaxAutreBat0 > 0 | SurfaceAutreBat0 > 0 | SurfaceParcAutreBat0 > 0) |>
+  select(interview__key, CapaciteMaxAutreBat0, SurfaceAutreBat0, SurfaceParcAutreBat0, NombrePoules0)
+rga23_prodAnimales |>
+  filter(CapaciteMaxBatSol0 > 0 | SurfaceBatSol0 > 0 | SurfaceParcBatSol0 > 0) |>
+  select(interview__key, CapaciteMaxBatSol0, SurfaceBatSol0, SurfaceParcBatSol0, NombrePoules0)
+rga23_prodAnimales |>
+  filter(CapaciteMaxCabMob0 > 0 | SurfaceCabMob0 > 0 | SurfaceParcCabMob0 > 0) |>
+  select(interview__key, CapaciteMaxCabMob0, SurfaceCabMob0, SurfaceParcCabMob0, NombrePoules0)
+
+### Plein air ou au sol
+rga23_prodAnimales |>
+  filter(CapaciteMaxAutreBat1 > 0 | SurfaceAutreBat1 > 0 | SurfaceParcAutreBat1 > 0) |>
+  select(interview__key, CapaciteMaxAutreBat1, SurfaceAutreBat1, SurfaceParcAutreBat1, NombrePoules1)
+rga23_prodAnimales |>
+  filter(CapaciteMaxBatSol1 > 0 | SurfaceBatSol1 > 0 | SurfaceParcBatSol1 > 0) |>
+  select(interview__key, CapaciteMaxBatSol1, SurfaceBatSol1, SurfaceParcBatSol1, NombrePoules1)
+rga23_prodAnimales |>
+  filter(CapaciteMaxCabMob1 > 0 | SurfaceCabMob1 > 0 | SurfaceParcCabMob1 > 0) |>
+  select(interview__key, CapaciteMaxCabMob1, SurfaceCabMob1, SurfaceParcCabMob1, NombrePoules1)
+
+### 3 - En cage
+rga23_prodAnimales |>
+  filter(CapaciteMaxAutreBat3 > 0 | SurfaceAutreBat3 > 0 | CapaciteMaxBatCage3 > 0 | SurfaceBatCage3 > 0) |>
+  select(interview__key, CapaciteMaxBatCage3, SurfaceBatCage3, CapaciteMaxAutreBat3, SurfaceAutreBat3, NombrePoules3)
+
+
+## Volailles
+rga23_prodAnimales |>
+  filter(SuperficieBatDeplVolailles > 0 | SuperficieBatFixesVolailles > 0) |>
+  select(
+    interview__key, SuperficieBatDeplVolailles, SuperficieBatFixesVolailles,
+    NbPoussins,
+    NbPoulettes,
+    NbPouletsChairCoqs,
+    NbCanards,
+    NbOies
+  )
+rga23_prodAnimales |>
+  filter(SurfaceParcoursVolailles > 0) |>
+  select(
+    interview__key,
+    SurfaceParcoursVolailles,
+    NbPoussins,
+    NbPoulettes,
+    NbPouletsChairCoqs,
+    NbCanards,
+    NbOies
+  )
