d6d0899c3c9cefb02332e1649d690f9525baf62b
Nathalie Dubreu <nathalie.dubreu@yahoo.fr>
2024-02-07 17:45:54 -1000

    Initialisation des programmes tape

---
+++ tape/1_Diversite.R
@@ -0,0 +1,78 @@
+# CULTURES
+# > 0 - Monoculture (ou aucune culture).
+# > 1 - Une culture couvrant plus de 80 pour cent de la surface cultivée.
+# > 2 - Deux ou trois cultures avec une superficie cultivée importante.
+# > 3 - Plus de 3 cultures avec une superficie cultivée importante adaptée aux conditions climatiques locales et changeantes.
+# > 4 - Plus de 3 cultures de variétés différentes adaptées aux conditions locales et ferme spatialement diversifiée avec multi, poly- ou interculture.
+
+rga23_surfacesCultures <- inner_join(inner_join(rga23_surfacesCultures, rga23_prodVegetales |> select(interview__key, SurfaceTotalProdAgri, SurfaceJardins)), rga23_tape |> select(interview__key, PratiquesCulturales__2)) |>
+  mutate(partCulture = SurfaceCult / SurfaceTotalProdAgri * 100)
+
+nbCulturesDeclarees <- rga23_surfacesCultures |>
+  count(interview__key, name = "nbCultures")
+
+scoreCultures <- left_join(rga23_surfacesCultures, nbCulturesDeclarees) |>
+  mutate(score = case_when(
+    nbCultures == 1 ~ 0,
+    partCulture > 80 ~ 1,
+    nbCultures == 2 | nbCultures == 3 ~ 2,
+    nbCultures > 3 & PratiquesCulturales__2 == 1 ~ 4,
+    nbCultures > 3 ~ 3.4
+  )) |>
+  group_by(interview__key, SurfaceTotalProdAgri, SurfaceJardins, nbCultures) |>
+  summarize(score = min(score))
+
+scoreCultures |>
+  group_by(score) |>
+  count()
+
+# ANIMAUX (Y COMPRIS LES POISSONS ET LES INSECTES)
+# > 0 - Aucun animal élevé.
+# > 1 - Une seule espèce élevée.
+# > 2 - Deux ou trois espèces, avec peu d’animaux.
+# > 3 – Plus de 3 espèces avec un nombre significatif d’animaux.
+# > 4 - Plus de 3 espèces de races différentes bien adaptées aux conditions climatiques locales et changeantes.
+
+presenceEspecesAnimaux <- paste0("PresenceAnimaux__", 1:8)
+
+rga23_prodAnimales <- rga23_prodAnimales %>%
+  mutate(nbEspeces = rowSums(across(
+    all_of(presenceEspecesAnimaux),
+    ~ coalesce(., 0)
+  )))
+
+scoreAnimaux <- rga23_prodAnimales |>
+  mutate(score = case_when(
+    nbEspeces == 0 ~ 0,
+    nbEspeces == 1 ~ 1,
+    nbEspeces == 2 | nbEspeces == 3 ~ 2,
+    nbEspeces > 3 ~ 3.4
+  )) 
+
+scoreAnimaux |>
+  group_by(score) |>
+  count()
+
+# ARBRES (ET AUTRES VIVACES)
+# > 0 - Pas d’arbres (ni d’autres plantes vivaces).
+# > 1 - Peu d’arbres (et / ou d’autres plantes vivaces) d’une seule.
+# > 2 - Certains arbres (et / ou autres plantes vivaces) de plus d’une espèce.
+# > 3 - Nombre important d’arbres (et / ou autres vivaces) d’espèces différentes.
+# > 4 - Nombre élevé d’arbres (et / ou autres plantes vivaces) de différentes espèces intégrées dans les terres agricoles.
+
+rga23_tape |> mutate(PresenceArbre = case_when(
+  PsceArbresHorsRente__1 == 1 & PsceArbresHorsRente__2 == 1 ~ "1 - Présents en bord de parcelle et dans la parcelle",
+  PsceArbresHorsRente__1 == 1  ~ "2 - Présents en bord de parcelle",
+  PsceArbresHorsRente__2 == 1 ~ "3 - Présents dans la parcelle",
+  PsceArbresHorsRente__3 == 1 ~ "4 - Absents",
+  TRUE ~ "Non concernés")) |>
+  group_by(PresenceArbre) |> count()
+
+
+# DIVERSITÉ DES ACTIVITÉS, PRODUITS ET SERVICES
+# > 0 - Une seule activité productive (par ex. vente d’une seule culture).
+# > 1 - Deux ou trois activités productives (par exemple, vendre 2 cultures ou une culture et un type d’animal).
+# > 2 - Plus de 3 activités productives.
+# > 3 - Plus de 3 activités productives et un service (par ex. transformation de produits à la ferme, écotourisme, transport de produits agricoles, formation, etc.).
+# > 4 - Plus de 3 activités productives et plusieurs services.
+
---
+++ tape/2_Synergies.R
@@ -0,0 +1,34 @@
+# 2. SYNERGIES
+# 
+# INTÉGRATION CULTURES-ÉLEVAGE-AQUACULTURE
+# L’énumérateur doit tenir compte des ressources partagées au niveau communautaire. Dans le cas des pâturages communaux par exemple, les intrants alimentaires correspondants pour les animaux ne sont pas considérés comme externes. Seuls les aliments achetés sur le marché sont considérés comme externes.
+# > 0 - Pas d’intégration: les animaux, y compris les poissons, sont nourris avec des aliments achetés et leur fumier n’est pas utilisé pour la fertilité du sol; OU il n’y a pas d’animaux dans l’agroécosystème.
+# > 1 - Faible intégration: les animaux sont principalement nourris avec des aliments achetés, leur fumier est utilisé comme engrais.
+# > 2 - Intégration moyenne: les animaux sont principalement nourris avec des aliments produits à la ferme et/ou au pâturage, leur fumier est utilisé comme engrais.
+# > 3 - Intégration élevée: les animaux sont principalement nourris avec des aliments produits à la ferme, des résidus de récolte et des sous-produits et / ou des pâturages, leur fumier est utilisé comme engrais et ils assurent un service (par ex. la traction).
+# > 4 - Intégration complète: les animaux sont exclusivement nourris avec des aliments produits à la ferme, des résidus de récolte et des sous-produits et / ou des pâturages, tout leur fumier est recyclé comme engrais et ils fournissent plus d’un service (par ex. nourriture, produits, traction, etc.).
+# 
+# GESTION DU SYSTÈME SOL-PLANTES
+# > 0 - Le sol est nu après la récolte. Pas de culture intercalaire. Aucune rotation des cultures (ou systèmes de pâturage en rotation). Perturbation importante du sol (biologique, chimique ou mécanique).
+# > 1 - Moins de 20 pour cent des terres arables sont couvertes de résidus ou de cultures de couverture. Plus de 80 pour cent des cultures sont produites en monoculture continue (ou sans pâturage en rotation).
+# > 2 - 50 pour cent du sol est recouvert de résidus ou de cultures de couverture. Certaines cultures sont tournées ou intercalées (ou un pâturage en rotation est effectué).
+# > 3 - Plus de 80 pour cent du sol est recouvert de résidus ou de cultures de couverture. Les cultures sont alternées régulièrement ou intercalées (ou le pâturage en rotation est systématique). La perturbation du sol est minimisée.
+# > 4 - Tout le sol est couvert de résidus ou de cultures de couverture. Les cultures sont tournées régulièrement et les cultures intercalaires sont courantes (ou le pâturage en rotation est systématique). Peu ou pas de perturbation du sol.
+# 
+# INTEGRATION AVEC LES ARBRES (AGROFORESTERIE, SILVOPASTORALISME, AGROSILVOPASTORALISME)
+# Considérez aussi les zones forestières communales.
+# > 0 - Pas d’intégration: les arbres (et autres plantes vivaces) n’ont pas de rôle pour l’homme ou dans la production végétale ou animale.
+# > 1 - Faible intégration: un petit nombre d’arbres (et autres plantes vivaces) ne fournissent qu’un seul produit (par exemple fruits, bois, fourrage, substances médicinales ou biopesticides…) ou qu’un seul service pour les cultures et/ou les animaux (par exemple de l’ombre pour les animaux, une fertilité accrue du sol, une rétention d’eau, une barrière à l’érosion du sol.
+# > 2 - Intégration moyenne: un nombre important d’arbres (et d’autres plantes vivaces) fournissent au moins un produit ou un service.
+# > 3 - Intégration élevée: un nombre important d’arbres (et autres plantes vivaces) fournissent plusieurs produits et services.
+# > 4 - Intégration complète: de nombreux arbres (et autres vivaces) fournissent plusieurs produits et services.
+
+# CONNECTIVITÉ ENTRE LES ÉLÉMENTS DE L’AGROÉCOSYSTÈME ET LE PAYSAGE
+# Considérez les zones environnantes, les environnements semi-naturels et les zones potentielles de compensation écologique.
+# > 0 - Pas de connectivité: grande uniformité à l’intérieur et à l’extérieur de l’agroécosystème, pas d’environnements semi-naturels, pas de zones de compensation écologique.
+# > 1 - Faible connectivité: quelques éléments isolés peuvent être trouvés dans l’agroécosystème, tels que des arbres, des arbustes, des clôtures naturelles, un étang ou une petite zone de compensation écologique.
+# > 2 - Connectivité moyenne: plusieurs éléments sont adjacents aux cultures et/ou pâturages ou à une grande zone de compensation écologique.
+# > 3 - Connectivité importante: plusieurs éléments peuvent être trouvés entre des parcelles de cultures et/ou des pâturages ou plusieurs zones de compensation écologique (arbres, arbustes, végétation naturelle, pâturages, haies, canaux, etc.).
+# > 4 - Connectivité élevée: l’agroécosystème présente une mosaïque et un paysage diversifié, de nombreux éléments tels que des arbres, des arbustes, des clôtures ou des étangs peuvent être trouvés entre chaque parcelle de terrain ou pâturage, ou plusieurs zones de compensation écologique.
+#                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                       
