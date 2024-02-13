846fc1a968296de006291158f640388723f18a52
Nathalie Dubreu <nathalie.dubreu@yahoo.fr>
2024-02-12 15:19:58 -1000

    Ajout tableaux par archipel partie Chef exploitation

--- analyse/chefExplEtMainOeuvre.R
+++ analyse/chefExplEtMainOeuvre.R
@@ -1,5 +1,8 @@
 ## Chefs d'exploitations
 
+ageParArchipel <- rga23_general |> group_by(Archipel_1) |> summarize(round(mean(age, na.rm = TRUE),2))
+writeCSV(ageParArchipel)
+
 genreChef <- rga23_general |>
   mutate(homme = case_when(SexeChefExpl == 1 ~ 0, SexeChefExpl == 2 ~ 1), femme = case_when(SexeChefExpl == 1 ~ 1, SexeChefExpl == 2 ~ 0)) |>
   summarize(
@@ -7,6 +10,16 @@ genreChef <- rga23_general |>
     NbFemmes = sum(femme, na.rm = TRUE),
     TauxFemmes = NbFemmes / NbHommes
   )
+
+genreChefArchipel <- rga23_general |>
+  mutate(homme = case_when(SexeChefExpl == 1 ~ 0, SexeChefExpl == 2 ~ 1), femme = case_when(SexeChefExpl == 1 ~ 1, SexeChefExpl == 2 ~ 0)) |>
+  group_by(Archipel_1) |>
+  summarize(
+    NbHommes = sum(homme, na.rm = TRUE),
+    NbFemmes = sum(femme, na.rm = TRUE),
+    TauxFemmes = NbFemmes / NbHommes
+  ) |> select(Archipel_1, TauxFemmes)
+writeCSV(genreChefArchipel)
 
 ### Temps de travail du chef d'exploitation
 
@@ -24,6 +37,22 @@ tempsTravailChef <- rga23_mainOeuvre |>
   mutate(`En %` = round(count / sum(count) * 100, 1)) |>
   select(`Temps de travail du chef d'exploitation`, `En %`)
 
+tempsTravailChefArchipel <- rga23_mainOeuvre |>
+  filter(!is.na(TpsTravailChefExpl)) |>
+  mutate(`Temps de travail du chef d'exploitation` = case_when(
+    (TpsTravailChefExpl == 1) ~ "1 : Moins de 1/2 temps",
+    (TpsTravailChefExpl == 2) ~ "2 : 1/2 temps",
+    (TpsTravailChefExpl == 3) ~ "3 : Entre 1/2 temps et temps complet",
+    (TpsTravailChefExpl == 4) ~ "4 : Temps complet",
+    (is.na(TpsTravailChefExpl)) ~ "Non réponse"
+  )) |>
+  group_by(`Temps de travail du chef d'exploitation`, Archipel_1) |>
+  summarise(count = n()) |>
+  mutate(`En %` = round(count / sum(count) * 100, 1)) |>
+  select(-count) |>
+  ungroup() |>
+  pivot_wider(names_from = `Temps de travail du chef d'exploitation`, values_from = `En %`)
+writeCSV(tempsTravailChefArchipel)
 
 formNAChefExpl <- rga23_general |>
   filter(!is.na(FormNAChefExpl)) |>
@@ -82,3 +111,79 @@ partRevenusAgriculture <- rga23_tape |>
   mutate(`En %` = round(count / sum(count) * 100, 1)) |>
   select(`Part de l'agriculture dans les revenus`, `En %`)
 
+## Tape
+
+# Vos besoins en nourriture et autres produits essentiels sont-ils satisfaits par votre production ou les revenus de votre production agricole ?
+besoinsNourritureArchipel <- rga23_tape |>
+  filter(!is.na(BesoinsSatisf)) |>
+  mutate(`Besoins satisfaits ?` = case_when(
+    BesoinsSatisf == 1 ~ "Oui",
+    BesoinsSatisf == 2 ~ "Non"
+  )) |>
+  group_by(Archipel_1, `Besoins satisfaits ?`) |>
+  summarise(count = n()) |>
+  mutate(`En %` = round(count / sum(count) * 100, 1)) |>
+  select(-count) |>
+  spread(key = `Besoins satisfaits ?`, value = `En %`)
+writeCSV(besoinsNourritureArchipel)  
+  
+# Les revenus de votre production agricole vous permettent-ils de réaliser des économies ?
+# Economies
+# Oui, de façon régulière.......1
+# Oui, de façon occasionnelle...2
+# Non...........................3
+economiesRevenusArchipel <- rga23_tape |>
+  filter(!is.na(BesoinsSatisf)) |>
+  mutate(`Economies ?` = case_when(
+    Economies == 1 ~ "Oui, de façon régulière",
+    Economies == 2 ~ "Oui, de façon occasionnelle",
+    Economies == 3 ~ "Non",
+    BesoinsSatisf == 2 ~ "Non (besoins non satisfaits)"
+  )) |>
+  group_by(Archipel_1, `Economies ?`) |>
+  summarise(count = n()) |>
+  mutate(`En %` = round(count / sum(count) * 100, 1)) |>
+  select(-count) |>
+  spread(key = `Economies ?`, value = `En %`)
+writeCSV(economiesRevenusArchipel)  
+
+# Les revenus de vos productions agricoles sont :
+# RevenusExpl
+# Stables.........................1
+# En diminution avec le temps.....2
+# En augmentation avec le temps...3
+# Pas de revenu...................4
+
+revenusProdAgricolesArchipel <- rga23_tape |>
+  filter(!is.na(RevenusExpl)) |>
+  mutate(`Les revenus de vos productions agricoles sont ?` = case_when(
+    RevenusExpl == 1 ~ "Stables",
+    RevenusExpl == 2 ~ "En diminution avec le temps",
+    RevenusExpl == 3 ~ "En augmentation avec le temps",
+    RevenusExpl == 4 ~ "Pas de revenu"
+  )) |>
+  group_by(Archipel_1, `Les revenus de vos productions agricoles sont ?`) |>
+  summarise(count = n()) |>
+  mutate(`En %` = round(count / sum(count) * 100, 1)) |>
+  select(-count) |>
+  spread(key = `Les revenus de vos productions agricoles sont ?`, value = `En %`)
+writeCSV(revenusProdAgricolesArchipel)  
+
+# Quelle est votre capacité de rétablissement après les chocs / perturbations (ex : inondation, sécheresse, maladie, ...) ?
+# Pas de capacité...1
+# Faible capacité...2
+# Bonne capacité....3
+
+capaciteRetablissementArchipel <- rga23_tape |>
+  filter(!is.na(CapaciteRecup)) |>
+  mutate(`Capacité de rétablissement après les chocs ?` = case_when(
+    CapaciteRecup == 1 ~ "Pas de capacité",
+    CapaciteRecup == 2 ~ "Faible capacité",
+    CapaciteRecup == 3 ~ "Bonne capacité"
+  )) |>
+  group_by(Archipel_1, `Capacité de rétablissement après les chocs ?`) |>
+  summarise(count = n()) |>
+  mutate(`En %` = round(count / sum(count) * 100, 1)) |>
+  select(-count) |>
+  spread(key = `Capacité de rétablissement après les chocs ?`, value = `En %`)
+writeCSV(capaciteRetablissementArchipel)  
--- analyse/publicationDoubleTimbre.R
+++ analyse/publicationDoubleTimbre.R
@@ -9,14 +9,14 @@ rga23_parcelles <- inner_join(readCSV("rga23_parcelles.csv"), rga23_champ |> sel
 rga23_prodVegetales <- inner_join(readCSV("rga23_prodVegetales.csv"), rga23_champ |> select(interview__key))
 rga23_prodAnimales <- inner_join(readCSV("rga23_prodAnimales.csv"), rga23_champ |> select(interview__key))
 rga23_surfacesCultures <- inner_join(readCSV("rga23_surfacesCultures.csv"), rga23_champ |> select(interview__key))
-rga23_tape <- inner_join(readCSV("rga23_tape.csv"), rga23_champ |> select(interview__key))
+rga23_tape <- inner_join(readCSV("rga23_tape.csv"), rga23_champ |> select(interview__key, Archipel_1))
 rga23_coprahculteurs <- inner_join(readCSV("rga23_coprahculteurs.csv"), rga23_champ |> select(interview__key, indicRGA23_Coprah))
 rga23_cocoteraies <- inner_join(readCSV("rga23_cocoteraies.csv"), rga23_champ |> select(interview__key))
 rga23_exploitations <- inner_join(readCSV("rga23_exploitations.csv"), rga23_champ |> select(interview__key, RaisonsRecensement__1, RaisonsRecensement__2))
-rga23_general <- inner_join(readCSV("rga23_general.csv"), rga23_champ |> select(interview__key)) |>
+rga23_general <- inner_join(readCSV("rga23_general.csv"), rga23_champ |> select(interview__key, Archipel_1)) |>
   mutate(age = 2023 - as.numeric(substring(DateNaissChefExpl, 7, 10)))
 rga23_mainOeuvre <- inner_join(readCSV("rga23_mainOeuvre.csv"), rga23_champ |>
-  select(interview__key)) |>
+  select(interview__key, Archipel_1)) |>
   mutate(totalMAOccas = ifelse(is.na(NbFemOccasAvecLien), 0, NbFemOccasAvecLien) +
     ifelse(is.na(NbFemOccasSansLien), 0, NbFemOccasSansLien) +
     ifelse(is.na(NbHomOccasAvecLien), 0, NbHomOccasAvecLien) +
