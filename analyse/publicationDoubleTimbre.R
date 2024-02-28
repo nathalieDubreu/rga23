a4daac31007d3c73e2b22e732a0ab892b7bbd72c
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-02-28 08:29:57 -1000

    Add files via upload

--- analyse/publicationDoubleTimbre.R
+++ analyse/publicationDoubleTimbre.R
@@ -2,18 +2,48 @@ library(tidyr)
 
 ## Restriction au champ 23 du RGA
 rga23_champ <- readCSV("rga23_gestion.csv") |>
-  filter(indicRGA23 == 1)
+  filter(indicRGA23 == 1) |>
+  mutate(TypeExploitation = case_when(
+    RaisonsRecensement__1 == 1 & RaisonsRecensement__2 == 0 & RaisonsRecensement__3 == 0 ~ "Cultivateurs seuls",
+    RaisonsRecensement__1 == 0 & RaisonsRecensement__2 == 1 & RaisonsRecensement__3 == 0 ~ "Eleveurs seuls",
+    RaisonsRecensement__1 == 0 & RaisonsRecensement__2 == 0 & RaisonsRecensement__3 == 1 ~ "Producteurs de coprah seuls",
+    TRUE ~ "Pluriactifs parmi cultures, élevages et coprah"
+  ))
 
 # Tables utiles - restreintes au champ
-rga23_parcelles <- inner_join(readCSV("rga23_parcelles.csv"), rga23_champ |> select(interview__key))
-rga23_prodVegetales <- inner_join(readCSV("rga23_prodVegetales.csv"), rga23_champ |> select(interview__key, Archipel_1, indicRGA23_Cultures, indicRGA23_Elevage, indicRGA23_Coprah))
-rga23_prodAnimales <- inner_join(readCSV("rga23_prodAnimales.csv"), rga23_champ |> select(interview__key, Archipel_1))
-rga23_surfacesCultures <- inner_join(readCSV("rga23_surfacesCultures.csv"), rga23_champ |> select(interview__key))
+rga23_parcelles <- inner_join(
+  readCSV("rga23_parcelles.csv"),
+  rga23_champ |> select(interview__key)
+)
+rga23_prodVegetales <- inner_join(
+  readCSV("rga23_prodVegetales.csv"),
+  rga23_champ |> select(interview__key, Archipel_1, indicRGA23_Cultures, indicRGA23_Elevage, indicRGA23_Coprah, TypeExploitation)
+)
+rga23_prodAnimales <- inner_join(
+  readCSV("rga23_prodAnimales.csv"),
+  rga23_champ |> select(interview__key, Archipel_1)
+)
+rga23_surfacesCultures <- inner_join(
+  readCSV("rga23_surfacesCultures.csv"),
+  rga23_champ |> select(interview__key)
+)
 rga23_tape <- inner_join(readCSV("rga23_tape.csv"), rga23_champ |> select(interview__key, Archipel_1))
-rga23_coprahculteurs <- inner_join(readCSV("rga23_coprahculteurs.csv"), rga23_champ |> select(interview__key, indicRGA23_Coprah))
-rga23_cocoteraies <- inner_join(readCSV("rga23_cocoteraies.csv"), rga23_champ |> select(interview__key))
-rga23_exploitations <- inner_join(readCSV("rga23_exploitations.csv"), rga23_champ |> select(interview__key, RaisonsRecensement__1, RaisonsRecensement__2))
-rga23_general <- inner_join(readCSV("rga23_general.csv"), rga23_champ |> select(interview__key, Archipel_1)) |>
+rga23_coprahculteurs <- inner_join(
+  readCSV("rga23_coprahculteurs.csv"),
+  rga23_champ |> select(interview__key, indicRGA23_Coprah)
+)
+rga23_cocoteraies <- inner_join(
+  readCSV("rga23_cocoteraies.csv"),
+  rga23_champ |> select(interview__key)
+)
+rga23_exploitations <- inner_join(
+  readCSV("rga23_exploitations.csv"),
+  rga23_champ |> select(interview__key, RaisonsRecensement__1, RaisonsRecensement__2)
+)
+rga23_general <- inner_join(
+  readCSV("rga23_general.csv"),
+  rga23_champ |> select(interview__key, Archipel_1, TypeExploitation)
+) |>
   mutate(
     age = 2023 - as.numeric(substring(DateNaissChefExpl, 7, 10)),
     homme = case_when(SexeChefExpl == 1 ~ 0, SexeChefExpl == 2 ~ 1),
@@ -23,16 +53,12 @@ rga23_general <- inner_join(readCSV("rga23_general.csv"), rga23_champ |> select(
       age < 60 ~ "2 - De 40 à moins de 60 ans",
       age >= 60 ~ "3 - 60 ans et plus",
       TRUE ~ "Non réponse"
-    ),
-    TypeExploitation = case_when(
-      RaisonsRecensement__1 == 1 & RaisonsRecensement__2 == 0 & RaisonsRecensement__3 == 0 ~ "Cultivateurs seuls",
-      RaisonsRecensement__1 == 0 & RaisonsRecensement__2 == 1 & RaisonsRecensement__3 == 0 ~ "Eleveurs seuls",
-      RaisonsRecensement__1 == 0 & RaisonsRecensement__2 == 0 & RaisonsRecensement__3 == 1 ~ "Producteurs de coprah seuls",
-      TRUE ~ "Pluriactifs parmi cultures, élevages et coprah"
     )
   )
-rga23_mainOeuvre <- inner_join(readCSV("rga23_mainOeuvre.csv"), rga23_champ |>
-  select(interview__key, Archipel_1, indicRGA23_Cultures, indicRGA23_Elevage, indicRGA23_Coprah)) |>
+rga23_mainOeuvre <- inner_join(
+  readCSV("rga23_mainOeuvre.csv"),
+  rga23_champ |> select(interview__key, Archipel_1, indicRGA23_Cultures, indicRGA23_Elevage, indicRGA23_Coprah, TypeExploitation)
+) |>
   mutate(totalMAOccas = ifelse(is.na(NbFemOccasAvecLien), 0, NbFemOccasAvecLien) +
     ifelse(is.na(NbFemOccasSansLien), 0, NbFemOccasSansLien) +
     ifelse(is.na(NbHomOccasAvecLien), 0, NbHomOccasAvecLien) +
