245227cae3de0eb1364e404a2ed36f048990e73a
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-03-14 14:13:37 -1000

    Add files via upload

--- analyse/SAU_horsCoco_horsPaturages.R
+++ analyse/SAU_horsCoco_horsPaturages.R
@@ -1,13 +1,16 @@
 rga23_surfacesCultures_horsCocoEtFourrag <- rga23_surfacesCultures |>
-  filter(TypeCulture != 80 & culture_id != 307 & culture_id != 308 & culture_id != 309)
+  filter(culture_id != 701 & culture_id != 702 & culture_id != 705 & culture_id != 307 & culture_id != 308 & culture_id != 309)
 
-surfacesParTypeHorsCocoFourragEtArchipel <- left_join(rga23_surfacesCultures_horsCocoEtFourrag, rga23_champ |> select(interview__key, Archipel_1)) |>
+surfacesParTypeHorsCocoFourragEtArchipel <- left_join(
+  rga23_surfacesCultures_horsCocoEtFourrag,
+  rga23_champ |> select(interview__key, Archipel_1)
+) |>
   left_join(rga23_mainOeuvre |> select(interview__key, SexeChefExpl)) |>
   group_by(Archipel_1, TypeCulture, SexeChefExpl) |>
   summarize(
     `Nb Exploitants` = n_distinct(interview__key),
     `Surface (m2)` = sum(SurfaceCult, na.rm = TRUE),
-    `Surface moyenne (m2)` = round(`Surface (m2)`/`Nb Exploitants`)
+    `Surface moyenne (m2)` = round(`Surface (m2)` / `Nb Exploitants`)
   )
 
 surfacesParTypeHorsCocoFourrag <- rga23_surfacesCultures_horsCocoEtFourrag |>
@@ -16,11 +19,14 @@ surfacesParTypeHorsCocoFourrag <- rga23_surfacesCultures_horsCocoEtFourrag |>
   group_by(Archipel_1, TypeCulture, SexeChefExpl) |>
   summarize(
     `Nb Exploitants` = n_distinct(interview__key),
-    `Surface (m2)` = sum(SurfaceCult, na.rm = TRUE), 
-    `Surface moyenne (m2)` = round(`Surface (m2)`/`Nb Exploitants`)
+    `Surface (m2)` = sum(SurfaceCult, na.rm = TRUE),
+    `Surface moyenne (m2)` = round(`Surface (m2)` / `Nb Exploitants`)
   )
 
-surfacesParTypeCultureArchipelEtTotalSexe <- rbind(surfacesParTypeHorsCocoFourragEtArchipel, surfacesParTypeHorsCocoFourrag) |>
+surfacesParTypeCultureArchipelEtTotalSexe <- rbind(
+  surfacesParTypeHorsCocoFourragEtArchipel,
+  surfacesParTypeHorsCocoFourrag
+) |>
   pivot_wider(names_from = c(Archipel_1, SexeChefExpl), values_from = c(`Nb Exploitants`, `Surface (m2)`, `Surface moyenne (m2)`), values_fill = 0)
 
 writeCSV(surfacesParTypeCultureArchipelEtTotalSexe)
