d93ef2ef36bc016f7fdd907084aef2f1ab214770
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-05-21 13:02:11 -1000

    Add files via upload

--- master.R
+++ master.R
@@ -13,6 +13,9 @@ source("analyse/2024-04-29_BSE_ComparaisonDonneesMaraichage.R")
 source("analyse/2024-05-13_BSE_TransformationProduitsCoco.R")
 source("analyse/2024-05-14_Ministere_ProportionSurfacesDispoCannabis.R")
 
+# TCD
+source("analyse/PreparationExportPourTCD.R")
+
 # Tape
 source("tape/tape.R")
 
