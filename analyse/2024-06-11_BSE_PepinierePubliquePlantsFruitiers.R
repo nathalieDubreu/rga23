da095091775724c1afda9c97f24c46198dccf176
nathalieDubreu <58486181+nathalieDubreu@users.noreply.github.com>
2024-06-11 09:56:38 -1000

    Add files via upload

---
+++ analyse/2024-06-11_BSE_PepinierePubliquePlantsFruitiers.R
@@ -0,0 +1,18 @@
+# Demande BSE : listing des interview__key des exploitants qui déclarent acheter des plants en pépinières publique + cultivent du fruitier
+
+cultivateursFruitiersPlantsPepinierePublique <- inner_join(
+  readCSV("rga23_general.csv") |> filter(indicPointsCAPL == 1),
+  readCSV("rga23_exploitations.csv") |> filter(ProvenancePlants__2 == 1) |> select(interview__key),
+  by = "interview__key"
+) |>
+  inner_join(readCSV("rga23_prodVegetales.csv") |> filter(CulturesPresentes__30 == 1 | CultPresentesJardins__30 == 1) |> select(interview__key),
+    by = "interview__key"
+  ) |>
+  select(interview__key, Archipel_1)
+
+writeCSV(cultivateursFruitiersPlantsPepinierePublique)
+
+## Pour info : répartition par archipel
+cultivateursFruitiersPlantsPepinierePublique |> 
+  group_by(Archipel_1) |>
+  count()
